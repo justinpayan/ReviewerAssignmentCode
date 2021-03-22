87f9e00162eb80d6376151a97ff6f9c8814642fa
justinpayan <jpayan@cs.umass.edu>
2021-03-22 12:17:30 -0400

    iter limit 1

--- pr4a_wrapper.py
+++ pr4a_wrapper.py
@@ -7,14 +7,14 @@ from autoassigner import *
 from utils import *
 
 
-def pr4a(pra, covs, loads):
+def pr4a(pra, covs, loads, iter_limit):
     # Normalize the affinities so they're between 0 and 1
     pra[np.where(pra < 0)] = 0
     pra /= np.max(pra)
 
     start = time.time()
 
-    pr4a_instance = auto_assigner(pra, demand=covs[0], ability=loads[0])
+    pr4a_instance = auto_assigner(pra, demand=covs[0], ability=loads[0], iter_limit=iter_limit)
     pr4a_instance.fair_assignment()
 
     alg_time = time.time() - start
@@ -30,8 +30,12 @@ if __name__ == "__main__":
     base_dir = args.base_dir
     dataset = args.dataset
 
+    iter_limit = np.inf
+    if dataset != "midl":
+        iter_limit = 1
+
     paper_reviewer_affinities = np.load(os.path.join(base_dir, dataset, "scores.npy"))
     covs = np.load(os.path.join(base_dir, dataset, "covs.npy"))
     loads = np.load(os.path.join(base_dir, dataset, "loads.npy")).astype(np.int64)
 
-    pr4a(paper_reviewer_affinities, covs, loads)
+    pr4a(paper_reviewer_affinities, covs, loads, iter_limit)
