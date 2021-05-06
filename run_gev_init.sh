223b05c255fb6b3ec8d85b0859c8d925b2bf1458
justinpayan <jpayan@cs.umass.edu>
2021-05-06 09:56:34 -0400

    mistyped some vars

--- run_gev_init.sh
+++ run_gev_init.sh
@@ -4,12 +4,12 @@ DATASET=$1
 BASE_DIR=$2
 NUM_PROCESSES=$3
 JOB_NUM=$4
-NUM_DISTRIBUTED_JOB=$5
+NUM_DISTRIB_JOBS=$5
 
 python greedy_based_on_expected_value.py --dataset $DATASET --base_dir $BASE_DIR \
                                          --num_processes $NUM_PROCESSES \
                                          --alloc_file gev_${DATASET} \
                                          --job_num $JOB_NUM \
-                                         --num_distrib_jobs $NUM_DISTRIBUTED_JOB \
+                                         --num_distrib_jobs $NUM_DISTRIB_JOBS \
                                          --mg_file mg_init_${DATASET} \
                                          --init_run
