3e81ee318f4ec7356e5b206f40aab80b14d3a21e
justinpayan <jpayan@cs.umass.edu>
2021-07-05 16:20:07 -0400

    allow for different seeds in final greedy algo

--- run_final_greedy.sh
+++ run_final_greedy.sh
@@ -3,7 +3,9 @@
 DATASET=$1
 BASE_DIR=$2
 NUM_PROCESSES=$3
+SEED=$4
 
 python final_greedy_algo.py --dataset $DATASET --base_dir $BASE_DIR \
                                          --num_processes $NUM_PROCESSES \
-                                         --alloc_file final_greedy_${DATASET}
+                                         --alloc_file final_greedy_${DATASET} \
+                                         --seed $SEED
