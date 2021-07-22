57c6efb50f17bed5eeeef4cde631b4e0a8434126
justinpayan <jpayan@cs.umass.edu>
2021-07-21 22:16:58 -0400

    more deletions

--- run_mallows_max_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-PHI=$1
-ITER=$2
-
-python mallows_max_test.py --phi $PHI --iter $ITER
--- run_tpms.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DATASET=$1
-BASE_DIR=$2
-
-python gurobi_usw_ef1.py --dataset $DATASET --base_dir $BASE_DIR
